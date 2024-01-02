2d117ecfed7cb468a0877e00128a306af43ef357
miku0129 <svartkatt39@gmail.com>
2024-01-02 21:59:25 +0100

    my first commit

---
+++ src/components/home/home.component.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import "./home.styles.scss";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <div className="home">
+        <span>Home</span>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ src/utilities/components.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderAndFooterLayout = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+`;
