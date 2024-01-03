01b0aaed027edb0b4e91ca2206153e5a9ad39b27
miku0129 <svartkatt39@gmail.com>
2024-01-03 22:05:25 +0100

    implement pagenation

--- src/components/articles-preview/articles-preview.component.tsx
+++ src/components/articles-preview/articles-preview.component.tsx
@@ -4,7 +4,6 @@ import ReactPaginate from "react-paginate";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Pagination from "react-bootstrap/Pagination";
 import "./articles-preview.styles.scss";
 
 import { dummy_articles } from "./__dummy-article-data__";
@@ -22,12 +21,29 @@ const ArticlesPreview = () => {
   function Items({ currentItems }: CurrentItems) {
     return (
       <div className="items">
-        {currentItems &&
-          currentItems.map((item) => (
-            <h3>
-              <span>{item.id}</span>: Item #{item.category}
-            </h3>
-          ))}
+        <Row xs={1} md={3} className="g-4">
+          {currentItems &&
+            currentItems.map((item, idx) => {
+              const text_intro = item.text.slice(0, 50);
+              return (
+                <Col key={idx}>
+                  <Card className="article-card">
+                    <Card.Img
+                      variant="top"
+                      src="https://images.unsplash.com/photo-1503676260728-1c00da094a0b?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100px160"
+                    />
+                    <Card.Body>
+                      <Card.Title>{item.title} </Card.Title>
+                      <p>
+                        <span>{item.id}/</span>category: {item.category}
+                      </p>
+                      <Card.Text>{text_intro}...</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+        </Row>
       </div>
     );
   }
@@ -48,7 +64,6 @@ const ArticlesPreview = () => {
       <>
         <Items currentItems={currentItems} />
         <ReactPaginate
-          breakLabel="..."
           nextLabel="next >"
           onPageChange={(e) => {
             // Invoke when user click to request another page.
@@ -59,9 +74,21 @@ const ArticlesPreview = () => {
             );
             setItemOffset(newOffset);
           }}
-          pageRangeDisplayed={5}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
           pageCount={pageCount}
           previousLabel="< previous"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
           renderOnZeroPageCount={null}
         />
       </>
@@ -71,30 +98,8 @@ const ArticlesPreview = () => {
   return (
     <Fragment>
       <div id="container">
-        <PaginatedItems itemsPerPage={3} />,
+        <PaginatedItems itemsPerPage={6} />,
       </div>
-      {/* <div>
-        <Row xs={1} md={3} className="g-4">
-          {articles.map((article, idx) => {
-            const text_intro = article.text.slice(0, 50);
-            return (
-              <Col key={idx}>
-                <Card>
-                  <Card.Img
-                    variant="top"
-                    src="https://images.unsplash.com/photo-1503676260728-1c00da094a0b?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100px160"
-                  />
-                  <Card.Body>
-                    <Card.Title>{article.title} </Card.Title>
-                    <p>category: {article.category}</p>
-                    <Card.Text>{text_intro}...</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </div> */}
     </Fragment>
   );
 };
