8b4cb00f260b10ee88d630f989dee14b2e57e1c3
miku0129 <svartkatt39@gmail.com>
2024-01-03 12:14:14 +0100

    add design to Footer

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -4,9 +4,26 @@ import "./footer.styles.scss";
 const Footer = () => {
   return (
     <Fragment>
-      <div className="footer">
-        <span>Footer</span>
-      </div>
+      <footer className="footer" id="footer">
+        <div className="viewport-container">
+          <div className="footer__content">
+            <section className="first-column">
+              <h2>Panser Nature</h2>
+              <ul>
+                <li>
+                  <span className="address">Address of Panser Nature</span>
+                </li>
+              </ul>
+            </section>
+          </div>
+          <div className="footer-logo-container"><p className="footer-logo">Facebook logo</p></div>
+        </div>
+        <div className="footer__disclaimer">
+          <div className="viewport-container">
+            <p>© 2023, Panser Nature</p>
+          </div>
+        </div>
+      </footer>
     </Fragment>
   );
 };
