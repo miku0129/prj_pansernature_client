adca8c515a94b109c3ec7ff5880e24673b506b01
miku0129 <svartkatt39@gmail.com>
2024-01-04 11:22:10 +0100

    add Article

--- src/components/article/article.component.tsx
+++ src/components/article/article.component.tsx
@@ -1,16 +1,39 @@
+import { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { ArticlesContext } from "../../context/articles.context";
 import { CustomBtn } from "../../utilities/components.styles";
+
+import { ArticleLayout } from "./article.styles";
 import "./article.styles.scss";
 
 const Article = () => {
   const navigate = useNavigate();
+
+  const articles = useContext(ArticlesContext)[0];
+  const params = useParams();
+  let id = params.id !== undefined ? +params.id : "";
+  let article = articles.find((article: Article) => {
+    return article.id === id;
+  });
+  console.log("article", article);
+
   return (
-    <div>
-      <h1>article</h1>
-      <CustomBtn type="button" onClick={() => navigate(-1)}>
-        Retour
-      </CustomBtn>
-    </div>
+    article && (
+      <ArticleLayout>
+        <div>
+          <h1>{article.title}</h1>
+          <hr />
+        </div>
+        <div>
+          <p>Category: {article.category}</p>
+          <p>Published date: {article.published_date}</p>
+          <text>{article.text}</text>
+        </div>
+        <CustomBtn type="button" onClick={() => navigate(-1)}>
+          Retour
+        </CustomBtn>
+      </ArticleLayout>
+    )
   );
 };
 
---
+++ src/components/article/article.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { HeaderAndFooterLayout } from "../../utilities/components.styles";
+
+export const ArticleLayout = styled(HeaderAndFooterLayout)`
+  padding: 20px;
+`;
