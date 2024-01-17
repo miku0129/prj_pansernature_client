b360f5f466e0f2ff329c98df19cadcaaeba87fc8
miku0129 <svartkatt39@gmail.com>
2024-01-17 17:05:25 +0100

    update Donation

--- src/components/donation/donation.component.tsx
+++ src/components/donation/donation.component.tsx
@@ -21,30 +21,35 @@ const Donation = () => {
             <Card.Img
               src="https://images.unsplash.com/photo-1483809715206-0499044b5b69?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Card image"
+              className="donation-card-img"
             />
           </div>
           <Card.ImgOverlay>
-            <Card.Title>Support the community</Card.Title>
-            <Card.Text>
-              Protect nature's beauty with a donation. Make a lasting impact on
-              biodiversity.
-            </Card.Text>
-            <div>
-              <Form.Label htmlFor="inputAmount">Amount of support</Form.Label>
-              <Form.Control
-                type="number"
-                id="inputAmount"
-                aria-describedby="amountHelpBlock"
-                placeholder="In 1 euro units"
-                onChange={(e) => setAmount(Number(e.target.value))}
-              />
-              <Card.Text id="amountHelpBlock">
-                Please enter the amount of your support.
-              </Card.Text>{" "}
+            <div className="donation-card-content-container">
+              <div>
+                <Card.Title>Support the community</Card.Title>
+                <Card.Text>
+                  Protect nature's beauty with a donation. Make a lasting impact
+                  on biodiversity.
+                </Card.Text>
+              </div>
+              <div>
+                <Form.Label htmlFor="inputAmount">Amount of support</Form.Label>
+                <Form.Control
+                  type="number"
+                  id="inputAmount"
+                  aria-describedby="amountHelpBlock"
+                  placeholder="In 1 euro units"
+                  onChange={(e) => setAmount(Number(e.target.value))}
+                />
+                <Card.Text id="amountHelpBlock">
+                  Please enter the amount of your support.
+                </Card.Text>{" "}
+              </div>
+              <CustomLink to={"checkout"} state={{ amount: amount }}>
+                <CustomBtn disabled={amount === 0}>Donate</CustomBtn>
+              </CustomLink>
             </div>
-            <CustomLink to={"checkout"} state={{ amount: amount }}>
-              <CustomBtn disabled={amount===0}>Donate</CustomBtn>
-            </CustomLink>
           </Card.ImgOverlay>
         </Card>
       </ContentLayout>
