b261a3e54ad629d3858d3a9408d7d4e18a6e5376
miku0129 <svartkatt39@gmail.com>
2024-01-25 15:27:30 +0100

    doc: update article-data

--- src/context/__dummy-article-data__.js
+++ src/context/__dummy-article-data__.js
@@ -11,7 +11,7 @@ export const dummy_articles = {
         "Panser la Nature, c'est Soigner la Vie",
       ],
       text: [
-        "Elle ne s'arrête pas où celle des autres commence, mais elle doit être également (répartie) respectée par tous, pour que tous les individus puissent en jouir. Le but de toute association politique est la conservation des droits naturels et imprescriptibles  de l'homme, (de la femme et de l'enfant) : Ces  *droits* sont *la Liberté, la Propriété, la Sûreté et la Résistance à l'oppression*. La liberté consiste à faire ce qui ne nuit pas à autrui ! Les représentants du peuple français doivent considérer (se rappeler) que l'ignorance, l'oubli ou le mépris des droits de l'homme et du citoyen, sont les seules causes des malheurs publics et de la corruption ! La *libre communication des pensées et des opinions est un des droits les plus précieux de L'homme*. Tout citoyen peut donc parler, écrire imprimer librement, sauf à répondre de l'abus de cette liberté.",
+        "Elle ne s'arrête pas où celle des autres commence, mais elle doit être également (répartie) respectée par tous, pour que tous les individus puissent en jouir. Le but de toute association politique est la conservation des droits naturels et imprescriptibles  de l'homme, (de la femme et de l'enfant) : Ces *droits* sont *la Liberté, la Propriété, la Sûreté et la Résistance à l'oppression*. La liberté consiste à faire ce qui ne nuit pas à autrui ! Les représentants du peuple français doivent considérer (se rappeler) que l'ignorance, l'oubli ou le mépris des droits de l'homme et du citoyen, sont les seules causes des malheurs publics et de la corruption ! La *libre communication des pensées et des opinions est un des droits les plus précieux de L'homme*. Tout citoyen peut donc parler, écrire imprimer librement, sauf à répondre de l'abus de cette liberté.",
         "*Le Libéralisme n'a rien à voir avec la déclaration des droits de l'homme !* C'est la négation de la liberté par la mainmise, l'accaparement, le despotisme et la tyrannie de tout ce qui appartient aux peuples et aux nations, par la corruption, l'empoisonnement et la déchéance des populations ainsi asservies. *C'est seulement la libre circulation de l'argent*, aux seuls profits des financiers, ceux qui les obtiennent, qui en profitent, ce sont les propriétaires, les commanditaires des sociétés multinationales, des industries chimiques, pétrolières, agroalimentaires, pharmaceutiques et phytosanitaires…",
         "Ces prédateurs n'ont de cesse que de détruire tout ce qui est d'origine naturelle pour le remplacer par des produis chimiques de synthèse, des poisons certifiés consommables, parce qu'ils génèrent plus de profits immédiats… Ce sont des gens sans foi ni loi, prêts à toute les compromissions, tous les crimes, pourvu que cela leur rapporte un gain rapide et incontrôlé, même au péril de leur propre avenir. Que des élus politiques, de par les hautes fonctions qu'ils occupent, s'arrogent le droit de promulguer des oukases, des décrets, qui privent les citoyens de leur droits les plus élémentaires, ceux de jouir des bienfaits de la nature, pour se plier aux dictats des prédateurs de la nature, devraient être considéré comme des renégats, ceux qui renient le vote de ceux qui les ont élus.  Le décret de Juillet 2006, stipule qu'il est interdit, entre autres (sous peine d'emprisonnement et de très fortes amendes injustifiées):",
         [
@@ -31,5 +31,65 @@ export const dummy_articles = {
         "C'est mon opinion et j'ai le droit de l'exprimer librement.",
       ],
     },
+    {
+      id: 1,
+      title: "On récolte ce que l’on sème",
+      category: "Agri-jardin",
+      published_date: "19/05/2012",
+      closing: ["Michel le Jardinier de PANSERNATURE"],
+      text: [
+        "Dès que juin et juillet arrivent, on peut profiter pleinement du jardin, surtout les fruits d'eau, quand la température s'y prête. Si l'an passé, après une fin d'hiver très chaude, on a eu une sérieuse avance, cette année est surtout frileuse, ce qui fait que légumes ont du retard, pour beaucoup. Que l'on se rassure, le temps va s'y mettre, et le potager va rattraper son retard.",
+        "En jardinerie, dans les grandes surfaces, les plants font leur apparition de plus en plus tôt dans la saison, souvent trop tôt, ce qui fait que bien des jardiniers se font avoir par les gelées tardives. Cette année, avec l'excès d'eau, le froid relatif, les plants sont soit retardés dans leur croissance, soit ils ont pourri sur pied ou dans le sol, il faut replanter.",
+        "Ce qui étonne les consommateurs, c'est de voir les étals envahis par l'appellation des graines et des plants « F1 ». La gamme F1 c'est une gamme de plants et de graines hybrides. Une forme de brevetage des plantes que les semenciers opèrent en agissant sur la génétique de la plante. On « marie » une plante du sud avec une du nord pour qu'elle puisse pousser aussi bien au nord qu'au sud, une forme d'uniformisation qui n'est pas sans conséquences sur sa qualité réelle, sur sa reproduction. (Le jardiner, l'agriculteur deviennent ainsi les otages du semencier).",
+        "On agit aussi sur la génétique en triturant la plante pour qu'elle devienne plus résistante aux maladies, aux insectes, en insérant des gènes étrangers (OGM), sans rien dévoiler ou expliquer. C'est une transgression de la Nature qui ne demande qu'à faire pousser des plantes naturelles, sur des sols qui ne subissent aucun des traumatismes opérés par l'industrie agroalimentaire.",
+        "Il faut absolument acheter les semis et les plants aux producteurs locaux, des maraîchers qui vendent leurs plantes sur les marchés, ou dans leurs serres, en vérifiant ce qu'ils ont fait. Biau Germe, Kokopelli, Semences Paysannes, font parti des organismes qui s'opposent à l'uniformité des plantes et à la dégénérescence orchestrée par les semenciers industriels.",
+        "Slow Food, Euro Gusto et l'université de Tours (IHEA) font la promotion du « Bien manger » dans des restaurants cotés et prétendent nous apprendre le goût des bonnes choses, mais n'indiquent pas ce qu'il faudrait faire pour obtenir des aliments de bonne qualité, ni leur provenance. Slow Food est partenaire  du consortium international « Terra »» qui prend le contrôle des plants de pommes de terre. Cela n'est pas anodin, adieu la Belle de Fontenay ou de Juillet, la Bintje qui sont considérées comme les reines du potager.",
+        "Un bon jardinier se doit d'être au courant et averti de ce qu'on lui prépare pour demain, parce qu'il a la charge de la santé de ceux qui se nourriront de ses légumes. De plus il surveille aussi la Lune, qui lui indique, selon sa position dans le ciel  (montante ou descendante) et la forme qu'elle présente, la nouvelle, les quartiers, la pleine, quand il doit semer ou planter pour aider la plante à mieux pousser, à être plus gustative.",
+        "C'est pour aider le jardinier (l'agriculteur aussi) que nous participons à des jardins partagés, comme à celui de la Vallée Violette de Joué-lès-Tours, au manoir de la Thibaudière de Chambray-lès-Tours, pour montrer les bons gestes, expliquer comment appliquer la culture sans pesticides.",
+        "N'hésitez pas à nous contacter, nous sommes aussi là pour vous.",
+      ],
+    },
+    {
+      id: 2,
+      title: "Les joies du jardinage",
+      category: "Agri-jardin",
+      published_date: "17/01/2011",
+      closing: ["Bonnes récoltes", "Michel le jardinier de PANSERNATURE"],
+      text: [
+        "En Mars, on commence à planter les légumes racines sur lesquels les derniers gels ne devraient pas agir. Semer sur côtière (un longue butte entre 2 sillons). Creuser un petit sillon dessus et y mettre une couche de terreau composté, semer en enfonçant légèrement  avec le doigt, une à deux graines tous les centimètres, (penser au volume pris à la récolte), puis recouvrir en ajoutant un mulch ou paillage. Dès que le semis a atteint 2 à 3 feuilles on l'éclaircit en enlevant ce qui est en surnombre et replanter ce qui a été retiré sur une nouvelle côtière. ",
+        "La plantation se fait en espaçant les légumes d'au moins 30 cm dans le rang et entre chaque rang :",
+        [
+          "Début Mars : On sème : hâtive, fève, navet, radis, raifort ; on plante : ail, artichaut, asperge, bulbe d'oignon, poireau, thym… ;",
+          "Fin Mars - début Avril : On plante: lentille, oignon, panais, pois, pomme de terre ;",
+          "Début Avril : On plante : épinard,  betterave, carotte, laitue à couper, persil, radis, salsifis… ;",
+          "En Mai : On pourra déjà commencer à récolter.",
+        ],
+        "*Une méthode de culture simplifiée* (Technique culturale simplifiée de Dominique Soltner) :",
+        [
+          "Après un désherbage à l'eau de pomme de terre (cuites avec la peau) ou au purin d'ortie pur, on dispose sur le sol un mulch épais composé d'une couche d'herbe, une légère couche de fumier, une couche de feuilles. Le tout doit avoir une épaisseur de  5 à 7 cm ;",
+          "On arrose le tout d'une tisane de jus d'ortie (un à 2 litres au m²), pour accélérer le compostage et on laisse agir au moins 2 mois ;",
+          "On plante les légumes, comme en pleine terre, dans le compost ainsi réalisé, on arrose si nécessaire, et on attend la récolte.",
+        ],
+        "Basé à Sainte-Gemmes-sur-Loire (49), Dominique cultive ainsi un hectare de maraîchage, sur un mulch de  foin, déposé en Novembre et planté en Mars. Il avoue avoir plus de temps passé à récolter qu'à planter.",
+        "*La Culture sur Mandala* : c'est un cercle divisé comme on le fait pour les parts d'un gâteau.",
+        [
+          "Ce cercle peut être de deux mètres, jusqu'à cent mètres de diamètre. L'ensemble est recouvert  comme ci-dessus ;",
+          "Chaque parcelle ainsi définie est destinée à une culture associée selon la nature des légumes, parce qu'il a été constaté que certains légumes se portent mieux lorsqu'ils sont cultivés à côté d'un autre, cela évite certaines maladies et cela augmente la production.",
+        ],
+        "Le mandala est un cercle créé selon une rite hindou pour augmenter la force des prières aux divinités, chaque division du cercle est chargé de symboles de couleur qui invitent à la réflexion. Par extension, la culture en mandala recherche les meilleures dispositions pouvant favoriser, par la couleur des plantes, des fleurs, une action dynamique pour les récoltes. On peut sur un balcon ou une terrasse, disposer les pots de culture en cercle. On y constate que ces récoltes sont aussi réussies que dans notre méthode de culture naturelle, qui a l'avantage de travailler sur les équilibres énergétiques.",
+      ],
+    },
+
+    // {
+    //   id: ,
+    //   title: "",
+    //   category: "",
+    //   published_date: "",
+    //   closing: [
+
+    //   ],
+    //   text: [
+    //   ],
+    // },
   ],
 };
