cad2abab4d02032063f90c171efea90d5a3bc1c7
miku0129 <svartkatt39@gmail.com>
2024-01-26 11:03:03 +0100

    feat: update Article

--- src/components/article/article.component.tsx
+++ src/components/article/article.component.tsx
@@ -13,15 +13,18 @@ const Article = () => {
   const articles = useContext(ArticlesContext)[0];
   const params = useParams();
   let id = params.id !== undefined ? +params.id : "";
-  let article = articles.find((article: Article) => {
-    return article.id === id;
-  });
-
+  let article = (() => {
+    if (articles) {
+      return articles.find((article: Article) => {
+        return article.id === id;
+      });
+    }
+  })();
 
   return (
     <ArticleLayout>
       <div>
-        {/* <h1>{article && article.title}</h1>
+        <h1>{article && article.title}</h1>
         <p>Category: {article && article.category}</p>
         <p>Published date: {article && article.published_date}</p>
         <hr />
@@ -29,7 +32,7 @@ const Article = () => {
       <div className="article-text-container">
         <div>
           {article &&
-            article.text.map((paragraph: string | string[], idx: number) =>
+            article.text.map((paragraph: string | ListOfArticle, idx: number) =>
               buildArticle(paragraph, idx)
             )}
         </div>
@@ -38,7 +41,7 @@ const Article = () => {
             article.closing.map((signature: string, idx: number) => (
               <p key={idx}>{signature}</p>
             ))}
-        </div> */}
+        </div>
       </div>
       <CustomBtn type="button" onClick={() => navigate(-1)}>
         Retour
