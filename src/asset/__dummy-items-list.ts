16234968ed5ba5f2b6352c24e4e4259af0d5f1f9
miku0129 <svartkatt39@gmail.com>
2024-09-10 16:55:50 +0200

    feat: update books.component.tsx

---
+++ src/asset/__dummy-items-list.ts
@@ -0,0 +1,47 @@
+export const dummy_items_list = {
+  data: [
+    {
+      item_id: 1,
+      title: "Notre méthode d'agriculture naturelle",
+      detail:
+        "Méthode d'Agriculture Naturelle est basée wur le respect des équilibres électroniques et minéraux situés dans l'humus.S de résacr Ce respect est incontournable alors qu'il n'est pas utilisé par les méthodes d'agricultues conventionnelle ou biologique.",
+      price: 12,
+      page: 48,
+      format: "15x21cm",
+      weight: 185,
+      type: "paper_book",
+      image_url: "https://i.ibb.co/6XYdvFx/LivrePN.gif",
+      created_date: "",
+      updated_date: "",
+    },
+    {
+      item_id: 2,
+      title: "sample book2",
+      detail:
+        "this is sample book2",
+      price: 100,
+      page: 200,
+      format: "15x21cm",
+      weight: 1000,
+      type: "e_book",
+      image_url: "https://i.ibb.co/6XYdvFx/LivrePN.gif",
+      created_date: "",
+      updated_date: "",
+    },
+    {
+      item_id: 3,
+      title: "sample book3",
+      detail:
+        "this is sample book3",
+      price: 100,
+      page: 200,
+      format: "15x21cm",
+      weight: 1000,
+      type: "e_book",
+      image_url: "https://i.ibb.co/6XYdvFx/LivrePN.gif",
+      created_date: "",
+      updated_date: "",
+    },
+
+  ],
+};
