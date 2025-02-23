ec90f5bf95fabe4441aba1b7b4d485e99e9d9b1c
miku0129 <svartkatt39@gmail.com>
2025-02-23 15:31:54 +0100

    refactor: updating News

--- src/routes/home.component.tsx
+++ src/routes/home.component.tsx
@@ -1,14 +1,16 @@
 import MainVisual from "../components/main-visual.component";
 import AboutUs from "../components/about-us.component";
 import AboutMichel from "../components/about-michel.component";
-import News from "../components/news/news.component";
+import News from "../components/news.component";
 import EntryPayasso from "../components/entry-payasso/entry-payasso.component";
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-1">
-      <MainVisual />
-      <AboutUs />
+    <div className="flex flex-col gap-4">
+      <div>
+        <MainVisual />
+        <AboutUs />
+      </div>
       <AboutMichel />
       <News />
       <EntryPayasso />
